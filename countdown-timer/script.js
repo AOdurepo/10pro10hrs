95aba0549bad55b1f3ea2781ee931277c2fa7fe0
AOdu <roddderich@gmail.com>
2023-12-14 19:24:24 -0800

    complete initial timer funtionality

--- countdown-timer/script.js
+++ countdown-timer/script.js
@@ -1,7 +1,10 @@
 const testDate = '30 January 2024';
 
 function countdown() {
-
-    const eventDate = new Date(csaExam);
+    const eventDate = new Date(testDate/*csaExam*/);
     const currentDate = new Date();
-}
+    console.log(eventDate - currentDate)
+} 
+
+// we should call the function at the end of this script
+countdown();
