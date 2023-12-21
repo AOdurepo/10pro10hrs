9dc7ef99248920e5c9b57777d5d93d1b8b737a18
AOdu <roddderich@gmail.com>
2023-12-20 22:51:45 -0800

    figuring countdown feature

--- countdown-timer/script.js
+++ countdown-timer/script.js
@@ -4,11 +4,16 @@ function countdown() {
     const eventDate = new Date(testDate/*csaExam*/);
     const currentDate = new Date();
     console.log(eventDate - currentDate)
+    
+    const seconds = (eventDate - currentDate ) * 10000
+
 } 
+
+countdown();
+
 
 setInterval(countdown, 1000)
 // we should call the function at the end of this script
-countdown();
 
 
 
