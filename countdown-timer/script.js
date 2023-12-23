ad70a173ea3171aabe5d1353529588962239a32c
AOdu <roddderich@gmail.com>
2023-12-23 13:22:18 -0800

    beginning conversions of days, hrs, mins, & secs time intervals

--- countdown-timer/script.js
+++ countdown-timer/script.js
@@ -3,17 +3,16 @@ const testDate = '30 January 2024';
 function countdown() {
     const eventDate = new Date(testDate/*csaExam*/);
     const currentDate = new Date();
-    console.log(eventDate - currentDate)
-    
-    const seconds = (eventDate - currentDate ) * 10000
+    //console.log(eventDate - currentDate)    
+    const seconds = (eventDate - currentDate ) * 1000;
+
+    const days = Math.floor(seconds / 3600 / 24);
+
+    console.log(days)
 
 } 
 
 countdown();
-
-
 setInterval(countdown, 1000)
 // we should call the function at the end of this script
-
-
 
