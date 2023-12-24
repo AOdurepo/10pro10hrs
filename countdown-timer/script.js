6012f8e2364c351119772649dcb9adbf8ad0d445
AOdu <roddderich@gmail.com>
2023-12-24 07:12:09 -0800

    completed minute & second conversions

--- countdown-timer/script.js
+++ countdown-timer/script.js
@@ -4,12 +4,14 @@ function countdown() {
     const eventDate = new Date(testDate/*csaExam*/);
     const currentDate = new Date();
     //console.log(eventDate - currentDate)    
-    const seconds = (eventDate - currentDate) / 1000 ;  //* 1000;
+    const totalSeconds = (eventDate - currentDate) / 1000 ;  //* 1000;
 
-    const days = Math.floor(seconds / 3600 / 24);
-    const hours = Math.floor(seconds / 3600) % 24;
+    const days = Math.floor(totalSeconds / 3600 / 24);
+    const hours = Math.floor(totalSeconds / 3600) % 24;
+    const minutes = Math.floor(totalSeconds / 60) % 60;
+    const seconds = Math.floor(totalSeconds % 60);
     //console.log(seconds);     
-    console.log(days, hours)
+    console.log(days, hours, minutes, seconds)
 
 } 
 
