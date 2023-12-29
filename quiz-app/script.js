afe21571127cef14bd4ab642e42eea1f698371f9
AOdu <roddderich@gmail.com>
2023-12-29 14:19:51 -0800

    built out quiz question object

---
+++ quiz-app/script.js
@@ -0,0 +1,47 @@
+const quizData = [
+    {
+        question: "What are 2 ways articles & questions are organized in the knowledge homepage?(all that apply)",
+        a: 'Article Rating',
+        b: 'Category',
+        c: 'Knwoledge Base',
+        d: 'Article Title',
+        correct: 'b' && 'c'
+        }, {
+        question: 'What are the steps for applying an update set an instance?',
+        a: 'Log',
+        b: 'Commit',
+        c: 'Debug',
+        d: 'Comment',
+        e: 'Retrieve',
+        f: 'Preview',
+        correct: 'b' && 'e' && 'f'
+        }, {
+        question: 'What type of scripts are supported for UI Policies??',
+        a: 'onCellEdit()',
+        b: 'onChange()',
+        c: 'onLoad()',
+        d: 'onSubmit()',
+        e: 'onRecords()',
+        f: 'onExit()',
+        correct: 'a' && 'b' && 'c' && 'd' && 'e'
+        }, {
+        question: 'What is used to add buttons, links, & context menu itesms on forms & lists?',
+        a: 'UI Actions',
+        b: 'Data Policies',
+        c: 'UI Policies',
+        d: 'Business Rules',
+        e: 'Update Set',
+        correct: 'a'
+        }, {
+        question: 'What is captured in an update set? (All that apply)',
+        a: 'New Data Records',
+        b: 'Tasks',
+        c: 'Client Scripts',
+        d: 'Business Rules',
+        e: 'Views',
+        f: 'New users & groups',
+        g: 'Tables',
+        h: 'Modified CIs', 
+        correct: 'c' && 'd' && 'e' && 'g'
+        }
+]
